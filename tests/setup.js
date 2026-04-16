3f7eb3267a584b1064aa3a6f58b9ca5462269caa
goldenvertex42 <fabled93@gmail.com>
2026-04-16 09:54:58 -0500

    chore(setup): configure vitest for setupFiles property in vite.config.js

---
+++ tests/setup.js
@@ -0,0 +1,9 @@
+import { expect, beforeAll, afterEach, afterAll } from 'vitest';
+import { server } from '../src/mocks/server';
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
