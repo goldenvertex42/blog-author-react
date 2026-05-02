e570433043ac717440c4274e9a6007c8ef7905d9
goldenvertex42 <fabled93@gmail.com>
2026-05-02 17:34:28 -0500

    chore: removing unnecessary code

--- src/pages/Register/RegisterPage.jsx
+++ src/pages/Register/RegisterPage.jsx
@@ -7,12 +7,9 @@ export async function registerAction({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
+  
   if (data.password !== data.confirmPassword) {
     return { errors: { confirmPassword: "Passwords do not match" } };
-  }
-  console.log(import.meta.env.ADMIN_SECRET_CODE);
-  if (data.adminCode !== import.meta.env.ADMIN_SECRET_CODE) {
-    return { errors: { adminCode: "Code incorrect; try again or register as reader" }}
   }
 
   try {
